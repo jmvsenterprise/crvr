474940cbbcc56cd0a64e27aa87ae0588d638add3
Joe Vrba <joeyvrba@live.com>
2023-06-18 21:53:05 -0400

    Added a request object and can parse out the type of request and the path. Next up, loading the requested file.

--- Server.cpp
+++ Server.cpp
@@ -1,15 +1,76 @@
 #include <assert.h>
 #include <winsock2.h>
 #include <stdio.h>
+#include <string>
+#include <iostream>
+
+namespace server {
+
+#define LEN(p) (sizeof(p) / sizeof(p[0]))
 
 const unsigned short port = 8080;
 
-int handle_client(SOCKET client, struct sockaddr_in *client_addr)
+enum request_type {
+	GET,
+	POST
+};
+
+struct request {
+	request_type type;
+	std::string path;
+
+	int parse(std::string &data);
+};
+
+int request::parse(std::string &data)
 {
+	size_t first_newline = data.find_first_of('\n');
+	if (data.npos == first_newline) {
+		std::cerr << "Did not find header EOL.\n";
+		return -1;
+	}
+
+	std::string header{data.substr(0, first_newline)};
+
+	size_t end_of_type = header.find_first_of(' ');
+	if (header.npos == end_of_type) {
+		std::cerr << "Did not find request type.\n";
+		return -1;
+	}
+
+	size_t end_of_path = header.find_first_of(' ', end_of_type + 1);
+	if (header.npos == end_of_path) {
+		std::cerr << "Did not find path.\n";
+		return -1;
+	}
+
+	std::string type{header.substr(0, end_of_type)};
+	path = header.substr(end_of_type + 1, end_of_path - end_of_type - 1);
+
+	std::cout << "Type: \"" << type << "\" path: \"" << path << "\"\n";
+
 	return -1;
 }
 
-int server(SOCKET server_sock)
+int handle_client(SOCKET client, struct sockaddr_in *client_addr)
+{
+	char buffer[4096] = {0};
+	int bytes_rxed = recv(client, buffer, LEN(buffer) - 1, 0);
+	if (bytes_rxed == SOCKET_ERROR) {
+		printf("Failed to read from client: %d.\n", WSAGetLastError());
+		return -1;
+	}
+
+	struct request request;
+	request.parse(std::string(buffer));
+
+	// Parse the data. But for now just print it out:
+	//printf("%s", buffer);
+
+	return -1;
+}
+
+int serve(SOCKET server_sock)
 {
 	int result = 0;
 	int keep_running = 1;
@@ -21,6 +82,12 @@ int server(SOCKET server_sock)
 		printf("contact detected.\n");
 		if (client != INVALID_SOCKET) {
 			assert(sizeof(client_addr) == addr_len);
+			printf("Contact: %d.%d.%d.%d:%u\n",
+				client_addr.sin_addr.S_un.S_un_b.s_b1,
+				client_addr.sin_addr.S_un.S_un_b.s_b2,
+				client_addr.sin_addr.S_un.S_un_b.s_b3,
+				client_addr.sin_addr.S_un.S_un_b.s_b4,
+				ntohs(client_addr.sin_port));
 			result = handle_client(client, &client_addr);
 			closesocket(client);
 
@@ -35,6 +102,8 @@ int server(SOCKET server_sock)
 	}
 	return result;
 }
+
+} // End namespace server
 
 int main()
 {
@@ -52,13 +121,13 @@ int main()
 		struct sockaddr_in address;
 		address.sin_family = AF_INET;
 		address.sin_addr.s_addr = inet_addr("127.0.0.1");
-		address.sin_port = htons(port);
+		address.sin_port = htons(server::port);
 
 		result = bind(server_sock, (SOCKADDR*)&address, sizeof(address));
 		if (0 == result) {
 			result = listen(server_sock, SOMAXCONN);
 			if (result != SOCKET_ERROR) {
-				result = server(server_sock);
+				result = server::serve(server_sock);
 			} else {
 				printf("Server socket failed to listen: %d.\n", WSAGetLastError());
 			}
