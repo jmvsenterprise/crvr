1b6cc77cdf3704ca62a517652bb8075c0ebe62fb
Joe Vrba <joeyvrba@live.com>
2023-06-20 21:18:47 -0400

    Working on adding str, pool, and str_list.

--- Server.cpp
+++ Server.cpp
@@ -1,7 +1,8 @@
 #include <assert.h>
+#include <errno.h>
+#include <stdint.h>
 #include <winsock2.h>
 #include <stdio.h>
-#include <string>
 #include <iostream>
 
 namespace server {
@@ -15,34 +16,195 @@ enum request_type {
 	POST
 };
 
-struct request {
-	request_type type;
-	std::string path;
-
-	int parse(std::string &data);
+struct pool {
+	size_t offset;
+	size_t cap;
+	char *buffer;
 };
 
-int request::parse(std::string &data)
+int pool_init(struct pool *p, size_t desired_size)
 {
-	size_t first_newline = data.find_first_of('\n');
-	if (data.npos == first_newline) {
-		std::cerr << "Did not find header EOL.\n";
+	assert(p && (p->buffer == NULL));
+	p->offset = 0;
+	p->cap = desired_size;
+	p->buffer = (char*)malloc(p->cap);
+
+	if (!p->buffer) {
+		return errno;
+	}
+	return 0;
+}
+
+void pool_free(struct pool *p)
+{
+	if (p && p->buffer) {
+		free(p->buffer);
+		p->buffer = NULL;
+	}
+	p->offset = p->cap = 0;
+}
+
+void *pool_alloc(struct pool *p, size_t byte_amount)
+{
+	size_t alignment = byte_amount % sizeof(void*);
+	byte_amount += alignment;
+	if ((p->offset + byte_amount) > p->cap) {
+		return NULL;
+	}
+	assert(p->buffer);
+	void *allocation = (void*)(p->buffer + p->offset);
+	p->offset += byte_amount;
+	return allocation;
+}
+
+#define pool_alloc_type(pool, type) (type*)pool_alloc(pool, sizeof(type))
+
+#define pool_alloc_array(pool, type, count) (type*)pool_alloc(pool, sizeof(type) * count)
+
+struct request {
+	request_type type;
+	char path[1024];
+	char format[1024];
+
+	int parse(char *data, const size_t len);
+};
+
+struct str {
+	size_t len;
+	char *data;
+};
+
+int str_find_substr(const struct str *target, const struct str *src)
+{
+	size_t src_char;
+	size_t target_char;
+	int found_location = -1;
+	int found = 0;
+	for (src_char = 0; src_char < src->len; ++src_char) {
+		if (src->data[src_char] == target->data[0]) {
+			assert(src_char < INT_MAX);
+			found_location = (int)src_char;
+			found = 1;
+			for (target_char = 1; (target_char < target->len) && (src_char < src->len); ) {
+				if (src->data[src_char] != target->data[target_char]) {
+					found = 0;
+					break;
+				}
+			}
+			if (found) {
+				return found_location;
+			}
+		}
+	}
+}
+
+int str_cmp(const struct str *a, const struct str *b)
+{
+	if (a->len > b->len) {
+		return -1;
+	} else if (a->len < b->len) {
+		return 1;
+	}
+	for (size_t i = 0; i < a->len; ++i) {
+		if (a->data[i] == b->data[i])
+			continue;
+		if (a->data[i] < b->data[i])
+			return 1;
+		return -1;
+	}
+	return 0;
+}
+
+int str_alloc(struct str *s, const size_t len, struct pool *p)
+{
+	assert(s && p);
+	if (len == 0) {
+		return 0;
+	}
+	s->len = len;
+	s->data = pool_alloc_array(p, char, s->len);
+	if (s->data) {
+		return 0;
+	}
+	return ENOMEM;
+}
+
+size_t str_cpy(const struct str *src, struct str *dest)
+{
+	size_t i = 0;
+	size_t max = (src->len > dest->len) ? dest->len : src->len;
+	for (i; i < max; ++i) {
+		dest->data[i] = src->data[i];
+	}
+	return i;
+}
+
+struct str_list {
+	struct str str;
+	struct str_list *next;
+};
+
+struct str_list *split(const struct str *src, const struct str *delimiter, struct pool *p)
+{
+	struct str_list *root = NULL;
+	struct str_list *end = NULL;
+	struct str_list *next = NULL;
+	int keep_going = 1;
+	int last = 0;
+
+	while (keep_going) {
+		int eow = str_find_substr(src, delimiter);
+		if (eow == -1) {
+			next = pool_alloc_type(p, struct str_list);
+			assert(eow > last);
+			int bytes_allocated = str_alloc(&next->str, eow - last, p);
+			if (bytes_allocated == ENOMEM) {
+				return NULL;
+			}
+			#error Copy characters to new str.
+			if (end) {
+				end->next = next;
+			} else if (root) {
+				end = root->next = next;
+			} else {
+				root = next;
+			}
+		}
+	}
+	return root;
+}
+
+int request::parse(const struct str *data, struct pool *p)
+{
+	char eol_chars[] = "\r\n";
+	const str eol = { strlen(eol_chars), eol_chars };
+	char space_chars[] = " ";
+	const str space = { strlen(space_chars), space_chars };
+	char get_chars[] = "GET";
+	const str get = { strlen(get_chars), get_chars };
+
+	struct str header;
+
+	int first_eol = str_find_substr(&eol, data);
+	if (first_eol == -1) {
+		printf("Did not find header EOL.\n");
 		return -1;
 	}
 
-	std::string header{data.substr(0, first_newline)};
-
-	size_t end_of_type = header.find_first_of(' ');
-	if (header.npos == end_of_type) {
-		std::cerr << "Did not find request type.\n";
+	if (str_alloc(&header, first_eol, p) != 0) {
+		printf("Failed to allocate header.\n");
 		return -1;
 	}
 
-	size_t end_of_path = header.find_first_of(' ', end_of_type + 1);
-	if (header.npos == end_of_path) {
-		std::cerr << "Did not find path.\n";
-		return -1;
+	size_t copies = str_cpy(data, &header);
+
+	struct str_list *tokens = split(&header, &space, p);
+	assert(tokens);
+	if (str_cmp(tokens->str, &get)) {
+		type = GET;
 	}
+
+	assert(strncpy_s(header, end_of_type, ))
 
 	std::string type{header.substr(0, end_of_type)};
 	path = header.substr(end_of_type + 1, end_of_path - end_of_type - 1);
