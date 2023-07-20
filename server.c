1e53a86bc7bd0519bb0b85734eb07e14a5beb326
Joey Vrba <joeyvrba@gmail.com>
2023-07-19 20:51:26 -0400

    Fixed most variables, but the cards variable is off by one and so it rips off the opening tag for a paragraph tag.

--- server.c
+++ server.c
@@ -509,14 +509,27 @@ int load_file(const char *file_name, char *buffer, const size_t buf_len,
 	return result;
 }
 
+/*
+ * Print the value of a variable to the buffer.
+ *
+ * buf - The buffer to replace the variable in.
+ * buf_len - The current length of valid data in the buffer.
+ * buf_cap - The max size the buffer can be. buf_len should be < buf_cap.
+ * var_name - The name of the variable being replaced.
+ * format - The format of the string to replace the variable with.
+ * ... - Additional arguments that go into the format.
+ *
+ * Returns 0 if the variable was successfully replaced in the buffer.
+ */
 int print_var_to(char *buf, size_t *buf_len, const size_t buf_cap,
-	const char *format, ...)
+	const char *var_name, const char *format, ...)
 {
 	va_list args;
 	int result;
 	char var_str[KILOBYTE] = {0};
 	size_t var_len;
 	size_t buf_space;
+	size_t var_name_len = strlen(var_name);
 
 	va_start(args, format);
 
@@ -539,11 +552,11 @@ int print_var_to(char *buf, size_t *buf_len, const size_t buf_cap,
 		return ENOBUFS;
 	}
 
-	#error need to remove the variable text from the buffer.
-
 	/* Move the rest of the buffer down and insert the variable. */
-	memmove(buf, buf + var_len, *buf_len + var_len);
-	*buf_len += var_len;
+	printf("var_len: %lu var_name_len: %lu *buf_len: %lu.\n", var_len,
+		var_name_len, *buf_len);
+	memmove(buf, buf + var_len + var_name_len, *buf_len + var_len);
+	*buf_len += var_len - var_name_len;
 
 	/* Write the variable in. */
 	memcpy(buf, var_str, var_len);
@@ -575,31 +588,31 @@ int replace_in_buf(char *buf, size_t buf_len, const size_t buf_cap)
 		if (memcmp(var_start + 1, card_var, STRMAX(card_var)) == 0) {
 			printf("Found cards var.\n");
 			result = print_var_to(var_start, &buf_len, buf_cap,
-				"%lu", quiz_len);
+				card_var, "%lu", quiz_len);
 		} else if (memcmp(var_start + 1, front_var, STRMAX(front_var))
 			== 0)
 		{
 			if (card->front) {
 				result = print_var_to(var_start, &buf_len,
-					buf_cap,
+					buf_cap, front_var,
 					"<img src=\"%s\" width=\"400\" height=\"400\">\n",
 					cards[card->card_id].file_name);
 			} else {
 				result = print_var_to(var_start, &buf_len,
-					buf_cap, "<p>%s</p>\n",
+					buf_cap, front_var, "<p>%s</p>\n",
 					cards[card->card_id].file_name);
 			}
 		} else if (memcmp(var_start + 1, back_var, STRMAX(back_var))
 			== 0)
 		{
 			if (!card->front) {
-				result = print_var_to(var_start + 1, &buf_len,
-					buf_cap,
+				result = print_var_to(var_start, &buf_len,
+					buf_cap, back_var,
 					"<img src=\"%s\" width=\"400\" height=\"400\">\n",
 					cards[card->card_id].file_name);
 			} else {
-				result = print_var_to(var_start + 1, &buf_len,
-					buf_cap, "<p>%s</p>\n",
+				result = print_var_to(var_start, &buf_len,
+					buf_cap, back_var, "<p>%s</p>\n",
 					cards[card->card_id].file_name);
 			}
 		}
