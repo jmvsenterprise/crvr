57613631ff0f60a37306427a055c339db948daa8
Joey Vrba <joeyvrba@gmail.com>
2023-07-25 08:00:06 -0400

    Figured out why the find_param function wasn't parsing the param correctly. sizeof('=') is 4, not 1. Have to cast to a char and now its working correctly it appears.

--- server.c
+++ server.c
@@ -696,26 +696,50 @@ int show_done_page(int client)
 int find_param(struct param out[static 1], struct request r[static 1],
 	const char *param_name)
 {
+	*out = (struct param){0};
+
 	if (!param_name)
 		return EINVAL;
 	char *param = strnstr(r->parameters, param_name, r->param_len);
 	if (!param) {
+		printf("Did not find %s in parameters.\n", param_name);
 		return EINVAL;
 	}
+
+	printf("param: \"%s\"\n", param);
+
 	size_t param_name_len = strlen(param_name);
 	(void)strncpy(out->name, param_name, param_name_len);
 
-	size_t param_value_offset = param_name_len + sizeof('=');
-	param += param_value_offset;
-	// This might be wrong. params might be separated by ;. need to verify.
-	char *eol = strnstr(param, "\r\n", r->param_len - param_value_offset);
-
-	// Assume eol is null
-	size_t value_len = r->param_len - param_value_offset;
-	if (eol != NULL) {
-		value_len = (size_t)(eol - param);
+	param += param_name_len;
+	printf("after name param: \"%s\"\n", param);
+	if ('=' != *param) {
+		printf("Param is not '=': \"%c\" (0x%hhx)\n", *param, *param);
+		return EINVAL;
 	}
-	(void)strncpy(out->value, param, value_len);
+	param += sizeof((char)'=');
+	printf("after = param: \"%s\"\n", param);
+
+	/*
+	 * Look for a \r\n or \0. If either one is encountered that's the end
+	 * of the parameter value.
+	 */
+	size_t value_end = 0;
+	printf("Checking param: ");
+	for (; param[value_end]; ++value_end) {
+		printf("'%c', ", param[value_end]);
+		if (('\n' == param[value_end]) && (value_end > 0) &&
+				(param[value_end - 1] == '\r')) {
+			value_end -= (sizeof((char)'\r') + sizeof((char)'\n'));
+			break;
+		}
+	}
+	printf("\n");
+
+	for (size_t i = 0; i < value_end; ++i) {
+		out->value[i] = param[i];
+	}
+	printf("param %s:%s.\n", out->name, out->value);
 
 	return 0;
 }
